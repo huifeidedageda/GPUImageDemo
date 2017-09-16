cae7e07b0d8923087df1ea10de7fb916e5d8b80b
SongZhiheng <szh13598845390@126.com>
2017-09-16 10:19:32 +0800

    测试了一下GPUImage滤镜的效果

--- SZH_GPUImageDemo/SZH_GPUImageDemo/ViewController.m
+++ SZH_GPUImageDemo/SZH_GPUImageDemo/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <GPUImage.h>
 @interface ViewController ()
 
 @end
@@ -17,6 +17,41 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+}
+
+
+#pragma mark ----------------- 
+
+
+#pragma mark -----------------  滤镜测试
+
+- (void)testGPUImage {
+    
+    UIImage *image  = [UIImage imageNamed:@"x"];
+    
+    
+    //GPUImage 滤镜
+    GPUImageLanczosResamplingFilter *disFilter = [[GPUImageLanczosResamplingFilter alloc] init];
+    
+    [disFilter forceProcessingAtSize:self.view.frame.size];
+    [disFilter useNextFrameForImageCapture];
+    
+    //获取数据源
+    GPUImagePicture *stillImageSource  = [[GPUImagePicture alloc]initWithImage:image];
+    
+    //添加滤镜
+    [stillImageSource addTarget:disFilter];
+    //开始渲染
+    [stillImageSource processImage];
+    //获取渲染后的图片
+    UIImage *newImage  = [disFilter imageFromCurrentFramebuffer];
+    
+    UIImageView *imageView = [[UIImageView alloc]initWithImage:newImage];
+    imageView.frame = self.view.bounds;
+    [self.view addSubview:imageView];
+    
 }
 
 
